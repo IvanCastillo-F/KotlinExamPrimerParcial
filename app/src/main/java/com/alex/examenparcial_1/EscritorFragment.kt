c8e2238bee71c780cf66797a3941227fdd6d2f4c
Ivan <alex9012345678@gmail.com>
2021-09-14 21:18:52 -0500

    Segunda Vista
    Se hizo la segunda vista aparte el fragmento login inicia cuando se carga la aplicación.

---
+++ app/src/main/java/com/alex/examenparcial_1/EscritorFragment.kt
@@ -0,0 +1,11 @@
+package com.alex.examenparcial_1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class EscritorFragment : Fragment(R.layout.fragment_escritor) {
+
+}
--- app/src/main/java/com/alex/examenparcial_1/MainActivity.kt
+++ app/src/main/java/com/alex/examenparcial_1/MainActivity.kt
@@ -2,10 +2,46 @@ package com.alex.examenparcial_1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.constraintlayout.widget.*
+import androidx.fragment.app.Fragment
 
 class MainActivity : AppCompatActivity() {
+
+    private val KEY = "STATE_KEY"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        replaceFragment(LoginFragment().apply {
+            arguments = Bundle().apply {
+                putString("key", "This is main fragment")
+
+            }
+        })
+
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.run {
+            putBoolean(KEY, false)
+        }
+
+        super.onSaveInstanceState(outState)
+    }
+
+
+    private fun replaceFragment(fragment: Fragment) {
+        supportFragmentManager.beginTransaction().apply {
+          /*  setCustomAnimations(
+                androidx.constraintlayout.widget.R.anim.slide_in_right,
+                androidx.constraintlayout.widget.R.anim.slide_out_left,
+                androidx.constraintlayout.widget.R.anim.slide_in_left,
+                androidx.constraintlayout.widget.R.anim.slide_out_right
+            )*/
+            replace(R.id.container, fragment)
+            addToBackStack(fragment.tag)
+            commit()
+        }
     }
 }

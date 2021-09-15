8af6642dfc0cf00e6156c51798006dde074db898
Ivan <alex9012345678@gmail.com>
2021-09-14 22:55:00 -0500

    Tercera Vista
    Se agrego la tercera vista de nuestra aplicación.

---
+++ app/src/main/java/com/alex/examenparcial_1/ViewArticleFragment.kt
@@ -0,0 +1,13 @@
+package com.alex.examenparcial_1
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ViewArticleFragment : Fragment(R.layout.fragment_view_article) {
+
+
+
+}
